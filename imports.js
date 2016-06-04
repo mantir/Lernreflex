7721ec8d9a967ca27d3be7b1ff0e996d4848e2d4
Martin <mkapp2@gmx.de>
2016-06-04 11:50:39 +0200

    Added Course

--- imports.js
+++ imports.js
@@ -10,13 +10,12 @@ export {default as InputScrollView} from 'reflect/components/InputScrollView'
 export {default as User} from 'reflect/models/User';
 export {default as Competence} from 'reflect/models/Competence';
 export {default as LearningTemplate} from 'reflect/models/LearningTemplate';
+export {default as Course} from 'reflect/models/Course';
 
 /* Components */
 export {default as CompetenceList} from 'reflect/components/CompetenceList';
 export {default as UserLogin} from 'reflect/components/UserLogin';
-/*export {default as Competence} from 'reflect/components/CompetenceList';
-export {default as CompetenceList} from 'reflect/components/CompetenceList';
-*/
+
 
 /* Tests */
 export {default as Test} from 'reflect/tests/Test';
