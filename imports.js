9452c3e3933a9178ab8d9874a695dacee5a0f6ff
Martin <mkapp2@gmx.de>
2016-10-11 12:05:30 +0200

    Added imports

--- imports.js
+++ imports.js
@@ -5,6 +5,7 @@ export {default as Router} from 'Lernreflex/Router';
 export {default as styles} from 'Lernreflex/styles';
 export {default as Loader} from 'Lernreflex/components/Loader';
 export {default as InputScrollView} from 'Lernreflex/components/InputScrollView'
+export {default as lib} from 'Lernreflex/lib';
 
 /* Models */
 export {default as User} from 'Lernreflex/models/User';
@@ -15,8 +16,10 @@ export {default as Activity} from 'Lernreflex/models/Activity';
 
 /* Components */
 export {default as CompetenceList} from 'Lernreflex/components/CompetenceList';
+export {default as SelectList} from 'Lernreflex/components/SelectList';
 export {default as CompetenceCreate} from 'Lernreflex/components/CompetenceCreate';
 export {default as CompetenceView} from 'Lernreflex/components/CompetenceView';
+export {default as Questions} from 'Lernreflex/components/Questions';
 export {default as ActivityView} from 'Lernreflex/components/ActivityView';
 export {default as UserLogin} from 'Lernreflex/components/UserLogin';
 export {default as Icon} from 'react-native-vector-icons/Ionicons';
